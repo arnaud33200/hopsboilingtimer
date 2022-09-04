877c65f7e341b6f311f40f7d5d1c05ae59bfdb40
Arnaud Ladoucette <arnaud@ritual.co>
2022-09-04 10:19:15 -0400

    setup view model for main screen

---
+++ app/src/main/java/ca/arnaud/hopsboilingtimer/app/screen/MainScreen.kt
@@ -0,0 +1,34 @@
+package ca.arnaud.hopsboilingtimer.app.screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ca.arnaud.hopsboilingtimer.app.theme.HopsBoilingTimerTheme
+
+@Composable
+fun MainScreen() {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HopsBoilingTimerTheme {
+        Greeting("Android")
+    }
+}
