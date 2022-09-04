78dc248cd4ea5ac1d14213ec892f261e5236fe9e
Arnaud Ladoucette <arnaud@ritual.co>
2022-09-04 15:24:14 -0400

    first screen layout, draft some ideas

--- app/src/main/java/ca/arnaud/hopsboilingtimer/app/screen/MainScreen.kt
+++ app/src/main/java/ca/arnaud/hopsboilingtimer/app/screen/MainScreen.kt
@@ -1,14 +1,24 @@
 package ca.arnaud.hopsboilingtimer.app.screen
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ca.arnaud.hopsboilingtimer.app.model.AdditionRowModel
 import ca.arnaud.hopsboilingtimer.app.model.MainScreenModel
 import ca.arnaud.hopsboilingtimer.app.theme.HopsBoilingTimerTheme
+import ca.arnaud.hopsboilingtimer.app.theme.Typography
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 interface MainScreenViewModel {
@@ -16,25 +26,137 @@ interface MainScreenViewModel {
 }
 
 @Composable
-fun MainScreen() {
-    // A surface container using the 'background' color from the theme
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
-    ) {
-        Greeting("Android")
+fun MainScreen(viewModel: MainScreenViewModel) {
+    val model = viewModel.screenModel.collectAsState().value
+    Scaffold(
+        topBar = {
+            Row(
+                modifier = Modifier
+                    .shadow(4.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    modifier = Modifier.padding(horizontal = 15.dp, vertical = 10.dp),
+                    text = "Hops Boiling Timer",
+                    style = Typography.h5
+                )
+            }
+        },
+        bottomBar = {
+            Column(
+                modifier = Modifier.padding(horizontal = 15.dp, vertical = 20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Button(
+                    // TODO - setup dimension
+                    modifier = Modifier
+                        .height(50.dp)
+                        .fillMaxWidth(),
+                    onClick = {}
+                ) {
+                    Row {
+                        Text(
+                            modifier = Modifier.weight(1f),
+                            text = "Start Timer"
+                        )
+                        Text("60 Min")
+                    }
+                }
+            }
+        }
+    ) { _ ->
+        Column(
+            modifier = Modifier.verticalScroll(rememberScrollState()),
+            content = {
+                model.additionRows.forEach { rowModel ->
+                    AdditionRow(model = rowModel)
+                }
+
+                AddNewAddition()
+            })
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AddNewAddition() {
+    Column(
+        Modifier.padding(horizontal = 15.dp, vertical = 20.dp),
+    ) {
+        Text(
+            "New Addition",
+            style = Typography.h6
+        )
+        Row(
+            // TODO - setup dimension in theme
+            modifier = Modifier.padding(vertical = 20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            TextField(
+                modifier = Modifier.weight(1f),
+                value = "",
+                label = { Text("Hops") },
+                onValueChange = {
+                    // TODO - add listener
+                }
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            TextField(
+                modifier = Modifier.defaultMinSize(60.dp),
+                value = "",
+                label = { Text("Min") },
+                onValueChange = {
+                    // TODO - add listener
+                }
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Icon(
+                modifier = Modifier.padding(5.dp),
+                imageVector = Icons.Rounded.Add,
+                contentDescription = ""
+            )
+        }
+    }
+}
+
+@Composable
+fun AdditionRow(model: AdditionRowModel) {
+    Row(
+        // TODO - setup dimension in theme
+        modifier = Modifier.padding(horizontal = 15.dp, vertical = 20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // TODO - setup typography
+        Text(
+            modifier = Modifier.weight(1f),
+            text = model.title
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            text = model.duration
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Icon(Icons.Filled.Delete, "")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HopsBoilingTimerTheme {
-        Greeting("Android")
+        MainScreen(viewModel = object : MainScreenViewModel {
+            override val screenModel: StateFlow<MainScreenModel>
+                get() = MutableStateFlow(
+                    MainScreenModel(
+                        listOf(
+                            AdditionRowModel("Amarillo", "60"),
+                            AdditionRowModel("Mozaic", "45"),
+                            AdditionRowModel("Saaz", "5"),
+                            AdditionRowModel("El Dorado", "10"),
+                        )
+                    )
+                )
+
+        })
     }
 }
