2cad49cded3eb54b1f3a1d09a6fdaad898583585
Arnaud Ladoucette <arnaud@ritual.co>
2022-09-04 10:00:00 -0400

    setup application class

---
+++ app/src/main/java/ca/arnaud/hopsboilingtimer/app/HopsBoilingTimerApplication.kt
@@ -0,0 +1,9 @@
+package ca.arnaud.hopsboilingtimer.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HopsBoilingTimerApplication : Application() {
+
+}
