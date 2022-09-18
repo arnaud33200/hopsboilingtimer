85af50aeee9a38cc79308d479c340674e0aa521a
Arnaud Ladoucette <arnaud@ritual.co>
2022-09-18 17:08:26 -0400

    add id to additionAlert

--- app/src/main/java/ca/arnaud/hopsboilingtimer/domain/mapper/AdditionAlertListMapper.kt
+++ app/src/main/java/ca/arnaud/hopsboilingtimer/domain/mapper/AdditionAlertListMapper.kt
@@ -4,6 +4,7 @@ import ca.arnaud.hopsboilingtimer.domain.common.DataMapper
 import ca.arnaud.hopsboilingtimer.domain.model.Addition
 import ca.arnaud.hopsboilingtimer.domain.model.AdditionAlert
 import ca.arnaud.hopsboilingtimer.domain.provider.TimeProvider
+import java.util.*
 import javax.inject.Inject
 
 class AdditionAlertListMapper @Inject constructor(
@@ -19,17 +20,19 @@ class AdditionAlertListMapper @Inject constructor(
         val durationAdditionsMap = input.sortedByDescending { it.duration }.groupBy { it.duration }
 
         return durationAdditionsMap.keys.mapIndexed { index, duration ->
+            val id = UUID.randomUUID().toString()
             val additions = durationAdditionsMap[duration] ?: emptyList()
             val countDown = maxDuration - duration
             val triggerAt = timeProvider.getNowTimeMillis() + countDown.toMillis()
             when (index) {
-                0 -> AdditionAlert.Start(triggerAt, additions)
-                else -> AdditionAlert.Next(triggerAt, additions)
+                0 -> AdditionAlert.Start(id, triggerAt, additions)
+                else -> AdditionAlert.Next(id, triggerAt, additions)
             }
         }.toMutableList().apply {
             if (isNotEmpty()) {
                 val triggerAt = timeProvider.getNowTimeMillis() + maxDuration.toMillis()
-                add(AdditionAlert.End(triggerAt))
+                val id = UUID.randomUUID().toString()
+                add(AdditionAlert.End(id, triggerAt))
             }
         }
     }
--- app/src/main/java/ca/arnaud/hopsboilingtimer/domain/model/AdditionAlert.kt
+++ app/src/main/java/ca/arnaud/hopsboilingtimer/domain/model/AdditionAlert.kt
@@ -2,20 +2,32 @@ package ca.arnaud.hopsboilingtimer.domain.model
 
 sealed interface AdditionAlert {
 
+    val id: String
     val triggerAtTime: Long
 
     data class Start(
+        override val id: String,
         override val triggerAtTime: Long,
         val additions: List<Addition>,
     ) : AdditionAlert
 
     data class Next(
+        override val id: String,
         override val triggerAtTime: Long,
         val additions: List<Addition>,
     ) : AdditionAlert
 
     data class End(
+        override val id: String,
         override val triggerAtTime: Long,
     ) : AdditionAlert
+}
+
+fun AdditionAlert.additionsOrEmpty(): List<Addition> {
+    return when (this) {
+        is AdditionAlert.End -> emptyList()
+        is AdditionAlert.Next -> additions
+        is AdditionAlert.Start -> additions
+    }
 }
 
