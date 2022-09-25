c7f18abf63b77d670c34b98a97df0922f25bc6e9
Arnaud Ladoucette <arnaud@ritual.co>
2022-09-24 23:29:38 -0400

    put total time and countdown in the button

--- app/src/main/java/ca/arnaud/hopsboilingtimer/app/factory/MainScreenModelFactory.kt
+++ app/src/main/java/ca/arnaud/hopsboilingtimer/app/factory/MainScreenModelFactory.kt
@@ -1,18 +1,24 @@
 package ca.arnaud.hopsboilingtimer.app.factory
 
+import ca.arnaud.hopsboilingtimer.app.mapper.DurationTextMapper
+import ca.arnaud.hopsboilingtimer.app.mapper.RemainingTimeTextMapper
 import ca.arnaud.hopsboilingtimer.app.model.BottomBarModel
 import ca.arnaud.hopsboilingtimer.app.model.ButtonStyle
 import ca.arnaud.hopsboilingtimer.app.model.MainScreenModel
 import ca.arnaud.hopsboilingtimer.app.model.NewAdditionModel
 import ca.arnaud.hopsboilingtimer.domain.model.Addition
+import ca.arnaud.hopsboilingtimer.domain.model.AdditionAlert
 import ca.arnaud.hopsboilingtimer.domain.model.AdditionSchedule
 import ca.arnaud.hopsboilingtimer.domain.model.getNextAlert
 import ca.arnaud.hopsboilingtimer.domain.provider.TimeProvider
 import javax.inject.Inject
+import kotlin.time.Duration.Companion.milliseconds
 
 class MainScreenModelFactory @Inject constructor(
     private val rowModelFactory: RowModelFactory,
     private val timeProvider: TimeProvider,
+    private val durationTextMapper: DurationTextMapper,
+    private val remainingTimeTextMapper: RemainingTimeTextMapper
 ) {
 
     fun create(
@@ -33,19 +39,27 @@ class MainScreenModelFactory @Inject constructor(
                 null -> newAdditionModel
                 else -> null
             },
-            bottomBarModel = createBottomModel(schedule)
+            bottomBarModel = createBottomModel(schedule, additions)
         )
     }
 
-    private fun createBottomModel(schedule: AdditionSchedule?): BottomBarModel {
+    private fun createBottomModel(
+        schedule: AdditionSchedule?,
+        additions: List<Addition>,
+    ): BottomBarModel {
         return when (schedule) {
             null -> BottomBarModel(
                 buttonTitle = "Start Timer", // TODO - hardcoded string
+                buttonTime = durationTextMapper.mapTo(additions.maxOf { it.duration }),
                 buttonStyle = ButtonStyle.Start
             )
             else -> {
+                val nowTime = timeProvider.getNowTimeMillis()
+                val triggerAtTime = schedule.alerts.maxOf { it.triggerAtTime }
+                val remainingDuration = (triggerAtTime - nowTime).milliseconds
                 BottomBarModel(
                     buttonTitle = "Stop Timer", // TODO - hardcoded string
+                    buttonTime = remainingTimeTextMapper.mapTo(remainingDuration),
                     buttonStyle = ButtonStyle.Stop
                 )
             }
