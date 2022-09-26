b4beb46ece9011bee6d012ae6651f2be6dbd5b75
Arnaud Ladoucette <arnaud@ritual.co>
2022-09-25 21:59:55 -0400

    put view above keboard

--- app/src/main/java/ca/arnaud/hopsboilingtimer/app/MainActivity.kt
+++ app/src/main/java/ca/arnaud/hopsboilingtimer/app/MainActivity.kt
@@ -1,11 +1,14 @@
 package ca.arnaud.hopsboilingtimer.app
 
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.collectAsState
+import androidx.core.view.WindowCompat.setDecorFitsSystemWindows
 import ca.arnaud.hopsboilingtimer.app.screen.MainScreen
 import ca.arnaud.hopsboilingtimer.app.theme.HopsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,6 +20,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         setContent {
             val darkMode = viewModel.darkMode.collectAsState().value
             HopsAppTheme(
