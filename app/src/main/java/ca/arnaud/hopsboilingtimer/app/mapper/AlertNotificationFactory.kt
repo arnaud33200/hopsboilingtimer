41e6fd74e99d96f09ea46174a0ae122960724620
Arnaud Ladoucette <arnaud@ritual.co>
2022-10-02 14:57:14 -0400

    setup click action on notification

--- app/src/main/java/ca/arnaud/hopsboilingtimer/app/mapper/AlertNotificationFactory.kt
+++ app/src/main/java/ca/arnaud/hopsboilingtimer/app/mapper/AlertNotificationFactory.kt
@@ -3,10 +3,13 @@ package ca.arnaud.hopsboilingtimer.app.mapper
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import ca.arnaud.hopsboilingtimer.R
+import ca.arnaud.hopsboilingtimer.app.MainActivity
 import ca.arnaud.hopsboilingtimer.app.alarm.AdditionNotification
 import javax.inject.Inject
 
@@ -14,6 +17,7 @@ class AlertNotificationFactory @Inject constructor() {
 
     companion object {
         private const val CHANNEL_ID = "CHANNEL_ID"
+        private const val CONTENT_INTENT_REQUEST_CODE = 3750
     }
 
     fun create(additionNotification: AdditionNotification, context: Context): Notification {
@@ -22,7 +26,16 @@ class AlertNotificationFactory @Inject constructor() {
             setContentTitle("Hops Boiling Timer")
             setContentText(additionNotification.message)
             priority = NotificationCompat.PRIORITY_MAX
+            setContentIntent(getContentIntent(context))
         }.build()
+    }
+
+    private fun getContentIntent(context: Context): PendingIntent? {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        }
+        val flags = PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_CANCEL_CURRENT
+        return PendingIntent.getActivity(context, CONTENT_INTENT_REQUEST_CODE, intent, flags)
     }
 
     fun createChannel(context: Context) {
