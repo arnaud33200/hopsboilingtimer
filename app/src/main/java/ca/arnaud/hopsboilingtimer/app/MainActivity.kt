7e5462e8bf5490f7157fac30efd003d3298bb820
arnaud <arnaud33200@gmail.com>
2023-07-15 14:16:27 -0400

    compose navigation with animation

--- app/src/main/java/ca/arnaud/hopsboilingtimer/app/MainActivity.kt
+++ app/src/main/java/ca/arnaud/hopsboilingtimer/app/MainActivity.kt
@@ -5,12 +5,10 @@ import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import ca.arnaud.hopsboilingtimer.app.di.assistedviewmodel.AssistedViewModelProviderFactory
-import ca.arnaud.hopsboilingtimer.app.navigation.common.createViewModel
-import ca.arnaud.hopsboilingtimer.app.screen.MainScreen
+import ca.arnaud.hopsboilingtimer.app.navigation.ApplicationNavigationGraph
 import ca.arnaud.hopsboilingtimer.app.theme.HopsAppTheme
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -24,23 +22,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         setContent {
-            val viewModel = viewModelAssistedFactory.createViewModel<MainViewModel>(
-                args = savedInstanceState
-            )
+            val navController = rememberAnimatedNavController()
 
-            val darkMode by viewModel.darkMode.collectAsState()
+            // have an actually main view model with just the theme (for now)
+//            val viewModel = viewModelAssistedFactory.createViewModel<MainViewModel>(
+//                args = savedInstanceState
+//            )
+//            val darkMode by viewModel.darkMode.collectAsState()
+
+            val darkMode: Boolean? = null
             HopsAppTheme(
                 darkTheme = darkMode ?: isSystemInDarkTheme(),
             ) {
-                // TODO - setup navigation
-
-                val model by viewModel.screenModel.collectAsState()
-                val showRequestPermissionDialog by viewModel.showRequestPermissionDialog.collectAsState()
-
-                MainScreen(
-                    model = model,
-                    showRequestPermissionDialog = showRequestPermissionDialog,
-                    actionListener = viewModel,
+                ApplicationNavigationGraph(
+                    navController = navController,
+                    viewModelAssistedFactory = viewModelAssistedFactory
                 )
             }
         }
