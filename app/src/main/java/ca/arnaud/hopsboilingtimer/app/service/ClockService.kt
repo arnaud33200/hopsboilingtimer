da0fdd1846d941a0db68caaf7a8ab6e6927a4672
arnaud <arnaud33200@gmail.com>
2023-08-01 22:53:56 -0400

    simplify the clockservice

--- app/src/main/java/ca/arnaud/hopsboilingtimer/app/service/ClockService.kt
+++ app/src/main/java/ca/arnaud/hopsboilingtimer/app/service/ClockService.kt
@@ -6,12 +6,11 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.lastOrNull
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.seconds
 
 @Singleton
@@ -19,48 +18,34 @@ class ClockService @Inject constructor(
     private val coroutineScopeProvider: CoroutineScopeProvider
 ) {
 
-    private val initialDelay: Duration = Duration.ZERO
     private val period: Duration = 1.seconds
+    private val initialDelay: Duration = Duration.ZERO
+    private val totalDuration: Duration = 1.days
 
     private var progressDuration: Duration = Duration.ZERO
-    private var clockFlow: Flow<Duration>? = null
-
     private val tickFlow = MutableSharedFlow<Duration>()
-    private var currentJob: Job? = null
+    private var currentClockJob: Job? = null
 
     fun getTickFlow(): Flow<Duration> {
         return tickFlow
     }
 
     fun start() {
-        currentJob = coroutineScopeProvider.scope.launch {
-            if (clockFlow != null) {
-                return@launch
-            }
-            clockFlow = flow {
-                delay(duration = initialDelay)
-                progressDuration += initialDelay
-                while (true) {
-                    Log.d("ClockService", "tick")
-                    emit(progressDuration)
-                    delay(period)
-                    progressDuration += period
-                }
-            }.also { flow ->
-                flow.collect { duration ->
-                    tickFlow.emit(duration)
-                }
+        currentClockJob = coroutineScopeProvider.scope.launch {
+            delay(duration = initialDelay)
+            progressDuration += initialDelay
+            while (progressDuration < totalDuration) {
+                Log.d("ClockService", "tick")
+                tickFlow.emit(progressDuration)
+                delay(period)
+                progressDuration += period
             }
         }
     }
 
     fun reset() {
-        coroutineScopeProvider.scope.launch {
-            progressDuration = Duration.ZERO
-            clockFlow?.lastOrNull()
-            clockFlow = null
-            currentJob?.cancel()
-            currentJob = null
-        }
+        progressDuration = Duration.ZERO
+        currentClockJob?.cancel()
+        currentClockJob = null
     }
 }
