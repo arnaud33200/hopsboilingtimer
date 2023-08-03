31af41159ca3c93c0babb80f276aafb2a19ee0fa
arnaud <arnaud33200@gmail.com>
2023-08-03 14:15:36 -0400

    fix tests

--- app/src/test/java/ca/arnaud/hopsboilingtimer/app/model/AdditionAlertDataDefault.kt
+++ app/src/test/java/ca/arnaud/hopsboilingtimer/app/model/AdditionAlertDataDefault.kt
@@ -1,9 +1,11 @@
 package ca.arnaud.hopsboilingtimer.app.model
 
 import ca.arnaud.hopsboilingtimer.app.feature.alert.model.AdditionAlertData
+import ca.arnaud.hopsboilingtimer.app.feature.alert.model.AdditionAlertDataType
 import java.time.Duration
 
 val additionAlertDataDefault = AdditionAlertData(
     id = "",
     initialDelay = Duration.ZERO,
+    type = AdditionAlertDataType.Alert,
 )
