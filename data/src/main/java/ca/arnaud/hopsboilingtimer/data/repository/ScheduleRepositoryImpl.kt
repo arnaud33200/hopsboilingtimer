e009915dea26071158f9b62d9eb2cc780cd91ce3
arnaud <arnaud33200@gmail.com>
2023-08-07 10:09:31 -0400

    optional for first initial state and reorganize code

--- data/src/main/java/ca/arnaud/hopsboilingtimer/data/repository/ScheduleRepositoryImpl.kt
+++ data/src/main/java/ca/arnaud/hopsboilingtimer/data/repository/ScheduleRepositoryImpl.kt
@@ -8,23 +8,53 @@ import ca.arnaud.hopsboilingtimer.domain.repository.ScheduleRepository
 import ca.arnaud.hopsboilingtimer.domain.usecase.schedule.UpdateAdditionAlert
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import java.util.Optional
 import javax.inject.Inject
+import kotlin.jvm.optionals.getOrNull
 
 class ScheduleRepositoryImpl @Inject constructor(
     private val scheduleLocalDataSource: ScheduleLocalDataSource,
 ) : ScheduleRepository {
 
-    private var scheduleFlow: MutableStateFlow<AdditionSchedule?>? = null
+    private val scheduleFlow = MutableStateFlow<Optional<AdditionSchedule>?>(null)
     private val nextAdditionAlert = MutableStateFlow<AdditionAlert?>(null)
 
+    // region Schedule
+    @OptIn(ExperimentalStdlibApi::class)
     override suspend fun getScheduleFLow(): Flow<AdditionSchedule?> {
-        return scheduleFlow ?: MutableStateFlow(scheduleLocalDataSource.getSchedule()).also {
-            scheduleFlow = it
+        if (scheduleFlow.value == null) {
+            scheduleFlow.value = Optional.ofNullable(scheduleLocalDataSource.getSchedule())
+        }
+        return scheduleFlow.map { optional ->
+            optional?.getOrNull()
         }
     }
 
-    override suspend fun getNextAlertFLow(): Flow<AdditionAlert?> {
+    override suspend fun getSchedule(): AdditionSchedule? {
+        return getScheduleFLow().first()
+    }
 
+    override suspend fun setSchedule(schedule: AdditionSchedule) {
+        scheduleLocalDataSource.setSchedule(schedule)
+        updateScheduleFlow(schedule)
+    }
+
+    override suspend fun deleteSchedule(schedule: AdditionSchedule) {
+        scheduleLocalDataSource.deleteSchedule(schedule)
+        updateScheduleFlow(null)
+    }
+
+    private fun updateScheduleFlow(schedule: AdditionSchedule?) {
+        this.scheduleFlow.value = Optional.ofNullable(schedule)
+    }
+
+    // endregion
+
+    // region Alert
+
+    override suspend fun getNextAlertFLow(): Flow<AdditionAlert?> {
         return nextAdditionAlert
     }
 
@@ -34,32 +64,18 @@ class ScheduleRepositoryImpl @Inject constructor(
         }
     }
 
-    override suspend fun getSchedule(): AdditionSchedule? {
-        return scheduleFlow?.value
-    }
-
-    override suspend fun setSchedule(schedule: AdditionSchedule) {
-        scheduleLocalDataSource.setSchedule(schedule)
-        updateCachedSchedule(schedule)
-    }
-
-    override suspend fun deleteSchedule(schedule: AdditionSchedule) {
-        scheduleLocalDataSource.deleteSchedule(schedule)
-        updateCachedSchedule(null)
-    }
-
     override suspend fun updateAdditionAlert(
         newAlert: AdditionAlert
     ): Response<AdditionAlert, UpdateAdditionAlert.UpdateAdditionAlertException> {
         return scheduleLocalDataSource.updateAdditionAlert(newAlert).also { response ->
             when (response) {
-                is Response.Success -> updateCachedSchedule(response.data)
+                is Response.Success -> updateScheduleAlert(response.data)
                 is Response.Failure -> {} // No-op
             }
         }
     }
 
-    private suspend fun updateCachedSchedule(updatedAlert: AdditionAlert) {
+    private suspend fun updateScheduleAlert(updatedAlert: AdditionAlert) {
         val currentSchedule = getSchedule() ?: return
         val newAlerts = currentSchedule.alerts.toMutableList().apply {
             replaceAll { alert ->
@@ -69,10 +85,8 @@ class ScheduleRepositoryImpl @Inject constructor(
                 }
             }
         }
-        updateCachedSchedule(currentSchedule.copy(alerts = newAlerts))
+        updateScheduleFlow(currentSchedule.copy(alerts = newAlerts))
     }
 
-    private fun updateCachedSchedule(schedule: AdditionSchedule?) {
-        this.scheduleFlow?.value = schedule
-    }
+    // endregion
 }
